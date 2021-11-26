dcdedc9fe66669d0a3578d0ba5c8e2bdabb8e91f
sreeram315 <sreerammaram2@gmail.com>
2021-11-26 18:20:13 +0530

    Added utils | crop function

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+def cropImage(imagePath, aspectRatio, salientCoordinates):
+	from PIL import Image
+	originalImage = Image.open(imagePath)
+	width, height = originalImage.size
+	print(width, height)
+	finalWidth = (0.75 * width)
+	left = max(0, salientCoordinates[0] - (finalWidth/2))
+	right = left + finalWidth
+
+	finalHeight = (aspectRatio[1]*finalWidth)/aspectRatio[0]
+	top = max(0, salientCoordinates[1] - (finalHeight/2))
+	bottom = min(top + finalHeight, height)
+	print(top, bottom, left, right)
+	croppedImage = originalImage.crop((left, top, right, bottom))
+	croppedImage.show()
+	croppedImage.save("s.jpeg")
